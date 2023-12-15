c40dfee9f46c64fca35f10f4eb9f7d4f0265f343
Naeet <nathnaelmeonnen@gmail.com>
2023-12-15 02:40:50 -0800

    remove files

--- _putchar.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-/**
- * _putchar - printf
- * @string: to be prined
- */
-void _putchar(const char *string)
-{
-	write(STDOUT_FILENO, string, strlen(string));
-}
--- prompt.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-/**
- * display_prompt - prompt
- */
-void display_prompt(void)
-{
-	_putchar("Cisfun$ ");
-	fflush(stdout);
-}
