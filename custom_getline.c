913ba558d32a426c14a02135d6a5e90ff2e958c9
Naeet <nathnaelmeonnen@gmail.com>
2023-12-15 14:08:15 -0800

    -m

--- custom_getline.c
+++ custom_getline.c
@@ -6,7 +6,7 @@
  */
 char *_getline(void)
 {
-	static char buffer[READ_SIZE], *line = NULL, *newline_pos = memchr(buffer, '\n', buffer_index);
+	static char buffer[READ_SIZE], *line = NULL;
 	static size_t buffer_index, line_size = 0;
 		
 	buffer_index = 0;
@@ -31,7 +31,7 @@ char *_getline(void)
 			buffer_index = read_chars;
 		}
 
-
+		char *newline_pos = memchr(buffer, '\n', buffer_index);
 		size_t copy_chars = (newline_pos != NULL)
 		       ? (size_t)(newline_pos - buffer + 1) : buffer_index;
 
